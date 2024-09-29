424da61bab6b05c5ffd38f9e9781dd42b6d41893
Horicow <t.c.l.twitch@gmail.com>
2024-09-29 12:21:41 -0700

    completed hour 7

---
+++ Hour7 Part 1/Assets/Scripts/ImportantFunctions.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ImportantFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print ("start runs before an object Updates");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print ("this is called once a frame");
+    }
+}
